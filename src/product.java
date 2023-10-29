e0d721b57992506b01156536d8fd9ec8ef0ca972
Akash@5024 <akavishwakarma82@gmail.com>
2023-10-30 01:11:44 +0530

    DSA practice  commit commit commit changes

---
+++ src/product.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class product {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in) ;
+            System.out.println("Enter two numbers\n");
+            int a=sc.nextInt();
+            int b=sc.nextInt();
+            int prod=a*b;
+            System.out.println(prod);
+        }
+        
+    }
+    
+
---
+++ src/sum.java
@@ -0,0 +1,9 @@
+public class sum {
+    public static void main(String[] args) {
+        int a=25;
+        int b=34;
+        int summation=a+b;
+        System.out.println(summation);
+    }
+    
+}
