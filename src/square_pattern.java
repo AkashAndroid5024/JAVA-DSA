d0452ceeecdb12caea1d30e1b9838b2243f38292
Akash@5024 <akavishwakarma82@gmail.com>
2023-11-02 01:53:32 +0530

    Square Pattern Asterisk

---
+++ src/square_pattern.java
@@ -0,0 +1,21 @@
+/*     Program to print the square pattern :
+
+* * * * 
+* * * * 
+* * * * 
+* * * *    
+        
+*/
+public class square_pattern {
+    public static void main(String[] args) {
+        for(int i=0;i<4;i++)
+        {
+            for(int j=0;j<4;j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    
+}
