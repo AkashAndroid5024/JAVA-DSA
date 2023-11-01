4b7a9c8c0d1fbe83ba19e059ac8c036a515f88eb
Akash@5024 <akavishwakarma82@gmail.com>
2023-11-02 02:02:42 +0530

    reverse of a number

---
+++ src/num_reverse.java
@@ -0,0 +1,20 @@
+// Program to print the reverse of a number
+import java.util.Scanner;
+
+public class num_reverse {
+    public static void main(String[] args) {
+         Scanner sc=new Scanner(System.in);
+         System.out.println("Enter a number");
+         int num=sc.nextInt();
+         int a=num;
+         int c=0;
+         while(a > 0)
+         {
+            int d = a%10;
+            c=c*10+d; // to store the reverse of the number
+            a=a/10;
+         }
+         System.out.println("Reverse of the number"+num+"is : "+c);
+    }
+    
+}
