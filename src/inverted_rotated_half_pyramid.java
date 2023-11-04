cc8e6254745c1a9a96be2baf52f4492ff631e79d
Akash@5024 <akavishwakarma82@gmail.com>
2023-11-05 00:28:09 +0530

    inverted_rotated_half_pyramid

---
+++ src/inverted_rotated_half_pyramid.java
@@ -0,0 +1,22 @@
+public class inverted_rotated_half_pyramid {
+    public static void main(String[] args) {
+        for(int i=1;i<=10;i++)
+        {
+            for(int j=1;j<=10;j++)
+            {
+                if(i+j>10)
+                {
+                System.out.print("* ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    
+}
+
+    
+
