a657706e570114b5b1544902391f0a3340901bfb
Akash@5024 <akavishwakarma82@gmail.com>
2023-11-05 01:59:11 +0530

    Hollow Rhombus

---
+++ src/hollow_rhombus.java
@@ -0,0 +1,46 @@
+/* Program to print the hollow Rhombus pattern :
+                  * * * * * * * * * *
+                *                 *
+              *                 *
+            *                 *
+          *                 *
+        *                 *
+      *                 *
+    *                 *
+  *                 *
+* * * * * * * * * *
+                             */
+import java.util.Scanner;
+
+public class hollow_rhombus {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number of rows");
+        int n=sc.nextInt();
+        
+        
+        for(int i=1;i<=n;i++)
+            {
+                for(int j=1;j<=n-i;j++)
+                {
+
+                    System.out.print("  ");
+                   
+                    
+                }
+                for(int j=1;j<=n;j++)
+                {
+                    if(i==1 || j==1 || i==n || j==n)
+                    {
+                    System.out.print("* ");
+                    }
+                    else{
+                        System.out.print("  ");
+                    }
+                }
+                System.out.println();
+            }
+            sc.close();
+    }
+    
+}
