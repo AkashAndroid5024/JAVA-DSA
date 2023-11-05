10ad9b476f42a6a7c7d100bd3ad981c437431acf
Akash@5024 <akavishwakarma82@gmail.com>
2023-11-06 00:23:23 +0530

    Number Pyramid

--- src/palindromic_pattern.java
+++ src/palindromic_pattern.java
@@ -16,6 +16,7 @@ public class palindromic_pattern {
     Scanner sc=new Scanner(System.in);
         System.out.println("Enter the number of rows");
         int n=sc.nextInt();
+        sc.close();
         for(int i=1;i<=n;i++)
     {
         for(int j=1;j<=n-i;j++)
