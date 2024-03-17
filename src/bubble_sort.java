e25464e04cbef8eb89a978c96f695fea94912270
Akash@5024 <akavishwakarma82@gmail.com>
2024-03-17 20:36:38 +0530

    Bubble Sorting

---
+++ src/bubble_sort.java
@@ -0,0 +1,30 @@
+public class bubble_sort {
+    public static void main(String[] args) {
+        int a[]={5,4,1,3,2};
+        bubbleSort(a);
+        print(a);
+    }
+    public static void bubbleSort(int a[])
+    {
+        for(int i=0;i<a.length-1;i++)
+        {
+            for(int j=0;j<a.length-1-i;j++)
+            {
+                if(a[j]>a[j+1])
+                {
+                    int temp=a[j];
+                    a[j]=a[j+1];
+                    a[j+1]=temp;
+                }
+            }
+        }
+    }
+    public static void print(int a[])
+    {
+        for(int i=0;i<a.length;i++)
+        {
+            System.out.print(a[i]+" ");
+        }
+    }
+    
+}
